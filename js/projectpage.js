2c63c08d9e81ccba301ac3fbae77b9da29354f91
Hankaji <hankaji135@gmail.com>
2023-01-15 19:08:05 +0700

    added 2 other pages
    skill n job, project outcomes added along with some bug fixes

--- js/projectpage.js
+++ js/projectpage.js
@@ -1,8 +1,23 @@
+const NAVBAR = document.getElementById("nav-bar");
 const contentMap = document.querySelectorAll(".pj-des .content *:not(p)");
-const tableOfContent = document.querySelector(".pj-des .table-of-content .toc ul");
+const tableOfContent = document.querySelector(".pj-des #table-of-content .table-of-content-block .toc ul");
+const dropBox = document.getElementsByClassName("dropbox");
+const dropBox_options = document.querySelector('.dropdown .page-option');
+const dropBox_i = document.querySelector('.dropdown i');
+
+// navigation bar
+document.addEventListener('scroll', ()=>{
+    var scrollPosition = window.scrollY
+    // console.log(scrollPosition)
+    if (scrollPosition > 300) {
+        NAVBAR.classList.add("compact")
+    } else {
+        NAVBAR.classList.remove("compact")
+    }
+});
+
 
 // Making a table of content
-
 let newListItem;
 let newAcnhorItem;
 let newUListItem;
@@ -36,4 +51,12 @@ contentMap.forEach((item)=>{
         newListItem.appendChild(newAcnhorItem);
         newUListItem.appendChild(newListItem);
     }
-})
+});
+
+// drop box
+for (var i = 0; i < dropBox.length; i++) {
+    dropBox[i].addEventListener('click', ()=>{
+        dropBox_i.classList.toggle('active');
+        dropBox_options.classList.toggle('active')
+    });
+}
